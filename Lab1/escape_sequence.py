37fe3b7c22a642eaa0ea855c152efaf13729b113
bedinadixit <bedinadixit@gmail.com>
2026-08-14 23:34:54 +0530

    Add files via upload

---
+++ Lab1/escape_sequence.py
@@ -0,0 +1,6 @@
+# This program prints a simple receipt using escape sequences.
+
+print("Item\tPrice")
+print("Pen\t20")
+print("Notebook\t50")
+print("Pencil\t10")
---
+++ Lab1/string_manipulation.py
@@ -0,0 +1,12 @@
+# This program performs different operations on a full name.
+
+full_name = input("Enter your full name: ")
+
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
+print("Reversed:", full_name[::-1])
+print("Length:", len(full_name))
+
+# These are three different string methods used in the program.
+print("Title Case:", full_name.title())
+print("Capitalized:", full_name.capitalize())
